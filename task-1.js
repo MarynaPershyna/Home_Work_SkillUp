4f2db7334eac4c80d4ecb6072a5a1b780ad81238
Maryna <mv15121983@gmail.com>
2022-12-23 10:57:08 +0200

    add task_1

--- task-1.js
+++ task-1.js
@@ -1,3 +1,9 @@
 const name = "Генератор захисного поля";
-const price = 1000;
+//const price = 1000;
+const price = 2000;
+
+const message = `Обрано ${name}, ціна за штуку ${price} кредитів`
+ console.log(message)
+
+ 
 
