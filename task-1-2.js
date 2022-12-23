1dde0130585a87c026c23ea6d1e7e48816fff4cb
Maryna <mv15121983@gmail.com>
2022-12-23 11:17:08 +0200

    add task-1-2

---
+++ task-1-2.js
@@ -0,0 +1,11 @@
+const total = 100;
+const ordered = 150;
+
+let result
+
+if (ordered > 100 ){
+    result = `На складі недостатньо товарів!`
+} else {
+    result = `Замовлення оформлено, з вами зв'яжеться менеджер`
+}
+console.log (result)
